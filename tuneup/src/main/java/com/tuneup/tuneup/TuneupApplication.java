7f6bc53f8acdf9b62363b6b13137057cd9c6e7e8
andrewMorrison20 <amorrison20@qub.ac.uk>
2024-11-02 15:55:45 +0000

    Intial Commit

---
+++ tuneup/src/main/java/com/tuneup/tuneup/TuneupApplication.java
@@ -0,0 +1,13 @@
+package com.tuneup.tuneup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuneupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuneupApplication.class, args);
+	}
+
+}
