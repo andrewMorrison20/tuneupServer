38bdf7e419be778a4331ffe378407d624c2db5fe
andrewMorrison20 <158091930+andrewMorrison20@users.noreply.github.com>
2024-11-03 10:56:07 +0000

    Tuneup 29 add api for user registry (#4)
    * fix some syntax
    
    * disable CSRF until JWT implemented
    
    * Add basic validator

---
+++ tuneup/src/main/java/com/tuneup/tuneup/users/validators/AppUserValidator.java
@@ -0,0 +1,27 @@
+package com.tuneup.tuneup.users.validators;
+
+import com.tuneup.tuneup.users.dtos.AppUserDto;
+import com.tuneup.tuneup.users.exceptions.ValidationException;
+import com.tuneup.tuneup.users.repository.AppUser;
+import com.tuneup.tuneup.users.repository.AppUserRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppUserValidator {
+
+    private final AppUserRepository appUserRepository;
+
+    public AppUserValidator(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
+
+    public void validateAppUser(AppUserDto appUserDto){
+        checkAppUserId(appUserDto.getId());
+    }
+
+    private void checkAppUserId(Long appUserId) {
+       if(appUserRepository.existsById(appUserId)){
+           throw new ValidationException("App user already exists");
+       }
+    }
+}
