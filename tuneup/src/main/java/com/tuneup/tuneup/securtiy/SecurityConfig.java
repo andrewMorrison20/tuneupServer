00fabca1a260eebd4914deb9d361c1e5d3de7769
andrewMorrison20 <158091930+andrewMorrison20@users.noreply.github.com>
2024-11-15 17:38:15 +0000

    Add bcrypt (#12)

--- tuneup/src/main/java/com/tuneup/tuneup/securtiy/SecurityConfig.java
+++ tuneup/src/main/java/com/tuneup/tuneup/securtiy/SecurityConfig.java
@@ -40,7 +40,8 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
