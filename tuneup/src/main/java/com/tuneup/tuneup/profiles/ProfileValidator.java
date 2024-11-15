a094d270a68e5b323103aadd9bb954480731c702
andrewMorrison20 <amorrison20@qub.ac.uk>
2024-11-15 20:43:19 +0000

    adding profile validator

--- tuneup/src/main/java/com/tuneup/tuneup/profiles/ProfileValidator.java
+++ tuneup/src/main/java/com/tuneup/tuneup/profiles/ProfileValidator.java
@@ -1,12 +1,42 @@
 package com.tuneup.tuneup.profiles;
 
+import com.tuneup.tuneup.Instruments.Instrument;
+import com.tuneup.tuneup.Instruments.repositories.InstrumentRepository;
 import com.tuneup.tuneup.profiles.dtos.ProfileDto;
+import com.tuneup.tuneup.users.exceptions.ValidationException;
 import org.springframework.stereotype.Component;
+
+import java.util.Set;
 
 @Component
 public class ProfileValidator {
 
-    public void validatorProfileDto(ProfileDto profileDto) {
+    private final InstrumentRepository instrumentRepository;
 
+    public ProfileValidator(InstrumentRepository instrumentRepository) {
+        this.instrumentRepository = instrumentRepository;
+    }
+
+    public void validatorProfileDto(ProfileDto profileDto) {
+        validateDisplayName(profileDto.getDisplayName());
+        validateInstruments(profileDto.getInstruments());
+
+    }
+
+    private void validateInstruments(Set<Instrument> instruments) {
+        if (instruments == null || instruments.isEmpty()) {
+            throw new ValidationException("The instrument list is empty");
+        }
+        for (Instrument instrument : instruments) {
+           if(! instrumentRepository.existsById(instrument.getId())){
+               throw new ValidationException("The instrument with id " + instrument.getId() + " does not exist");
+           };
+        }
+    }
+
+    private void validateDisplayName(String displayName) {
+        if (displayName == null || displayName.trim().isEmpty()) {
+            throw new ValidationException("Display name cannot be empty");
+        }
     }
 }
