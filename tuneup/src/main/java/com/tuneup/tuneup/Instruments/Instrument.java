f1c1ea77c214f40237162ab4f3a22a0580ab8a9f
andrewMorrison20 <158091930+andrewMorrison20@users.noreply.github.com>
2024-11-15 16:58:40 +0000

    Tuneup 34 basic user profiles (#9)
    * begin profile api
    
    * begin profile api

---
+++ tuneup/src/main/java/com/tuneup/tuneup/Instruments/Instrument.java
@@ -0,0 +1,45 @@
+package com.tuneup.tuneup.Instruments;
+
+import com.tuneup.tuneup.profiles.Profile;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import java.util.Set;
+
+@Entity
+@Table(name = "instrument")
+public class Instrument {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "instruments")
+    private Set<Profile> profiles;
+
+    // Constructors, getters, and setters
+    public Instrument() {}
+
+    public Instrument(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public long getId(){
+        return this.id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+
+    }
+
+}
---
+++ tuneup/src/main/java/com/tuneup/tuneup/profiles/dtos/ProfileDto.java
@@ -0,0 +1,13 @@
+package com.tuneup.tuneup.profiles.dtos;
+
+import com.tuneup.tuneup.profiles.ProfileType;
+
+public class ProfileDto {
+
+    private int id;
+    private String displayName;
+    private String bio;
+    private boolean onlineLessons;
+    private ProfileType profileType;
+
+}
