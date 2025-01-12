1d886cb564e18745bab7e0736208c91616ac1ac2
andrewMorrison20 <amorrison20@qub.ac.uk>
2025-01-12 15:18:54 +0000

    make sure error message dissapears

--- src/main/java/com/tuneup/tuneup/profiles/repositories/ProfileSpecification.java
+++ src/main/java/com/tuneup/tuneup/profiles/repositories/ProfileSpecification.java
@@ -1,6 +1,7 @@
 package com.tuneup.tuneup.profiles.repositories;
 
 import com.tuneup.tuneup.genres.Genre;
+import com.tuneup.tuneup.pricing.Price;
 import com.tuneup.tuneup.profiles.Profile;
 import com.tuneup.tuneup.profiles.dtos.ProfileSearchCriteria;
 import com.tuneup.tuneup.regions.RegionRepository;
@@ -11,6 +12,9 @@ import com.tuneup.tuneup.Instruments.Instrument;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.util.Collections.max;
+import static java.util.Collections.min;
 
 
 public class ProfileSpecification {
@@ -34,6 +38,14 @@ public class ProfileSpecification {
                 predicates.add(genreJoin.get("id").in(criteria.getGenre()));
             }
 
+            if(criteria.getPriceRange()!=null){
+                Double lowerLimit = min(criteria.getPriceRange());
+                Double upperLimit = max(criteria.getPriceRange());
+
+                Join<Profile, Price> priceJoin = root.join("prices");
+                predicates.add(builder.between(priceJoin.get("rate"), lowerLimit, upperLimit));
+            }
+
             if (criteria.getInstruments() != null) {
                 Join<Profile, Instrument> instrumentJoin = root.join("instruments");
                 predicates.add(instrumentJoin.get("id").in(criteria.getInstruments()));
@@ -45,8 +57,8 @@ public class ProfileSpecification {
             if (criteria.getKeyword() != null) {
                 String keywordPattern = "%" + criteria.getKeyword().toLowerCase() + "%";
                 predicates.add(builder.or(
-                        //builder.like(builder.lower(root.get("tuitionRegion")), keywordPattern),
                         builder.like(builder.lower(root.get("displayName")), keywordPattern)
+
                 ));
             }
             return builder.and(predicates.toArray(new Predicate[0]));
