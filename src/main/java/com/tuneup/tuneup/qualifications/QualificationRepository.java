6231a1c7cf53e3db5778cff3c5d4812d893125e1
andrewMorrison20 <amorrison20@qub.ac.uk>
2025-01-12 16:44:18 +0000

    Adding qualifcations API

--- src/main/java/com/tuneup/tuneup/qualifications/QualificationRepository.java
+++ src/main/java/com/tuneup/tuneup/qualifications/QualificationRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface QualificationRepository extends JpaRepository<Qualification, Long> {
-    List<Qualification> findByInstrument_Name(String instrumentName);
+
 }
