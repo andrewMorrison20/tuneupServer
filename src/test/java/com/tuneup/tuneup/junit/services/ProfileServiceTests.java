7eb2f03a7fe26457b3910d9c673b76cd904cc4c7
andrewMorrison20 <amorrison20@qub.ac.uk>
2025-01-19 14:05:28 +0000

    add controller unit tests

--- src/test/java/com/tuneup/tuneup/junit/services/ProfileServiceTests.java
+++ src/test/java/com/tuneup/tuneup/junit/services/ProfileServiceTests.java
@@ -1,12 +1,20 @@
 package com.tuneup.tuneup.junit.services;
 
 
+import com.tuneup.tuneup.Instruments.Instrument;
+import com.tuneup.tuneup.Instruments.InstrumentService;
+import com.tuneup.tuneup.pricing.Price;
+import com.tuneup.tuneup.pricing.PriceDto;
+import com.tuneup.tuneup.pricing.PriceValidator;
 import com.tuneup.tuneup.profiles.Profile;
 import com.tuneup.tuneup.profiles.ProfileMapper;
 import com.tuneup.tuneup.profiles.ProfileService;
 import com.tuneup.tuneup.profiles.ProfileValidator;
 import com.tuneup.tuneup.profiles.dtos.ProfileDto;
 import com.tuneup.tuneup.profiles.repositories.ProfileRepository;
+import com.tuneup.tuneup.qualifications.Qualification;
+import com.tuneup.tuneup.qualifications.dtos.ProfileInstrumentQualificationDto;
+import com.tuneup.tuneup.qualifications.services.QualificationService;
 import com.tuneup.tuneup.users.model.AppUser;
 import com.tuneup.tuneup.users.services.AppUserService;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,8 +27,10 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -37,7 +47,16 @@ import static org.mockito.Mockito.*;
         private ProfileValidator profileValidator;
 
         @Mock
+        private QualificationService qualificationService;
+
+        @Mock
+        private PriceValidator priceValidator;
+
+        @Mock
         private AppUserService appUserService;
+
+        @Mock
+        private InstrumentService instrumentService;
 
         @InjectMocks
         private ProfileService profileService;
@@ -77,7 +96,6 @@ import static org.mockito.Mockito.*;
 
         @Test
         void createProfile_shouldValidateSaveAndReturnDto() {
-            // Arrange
             ProfileDto inputDto = new ProfileDto();
             inputDto.setAppUserId(1L);
 
@@ -91,10 +109,8 @@ import static org.mockito.Mockito.*;
             when(profileRepository.save(mockProfile)).thenReturn(savedProfile);
             when(profileMapper.toProfileDto(savedProfile)).thenReturn(expectedDto);
 
-            // Act
             ProfileDto result = profileService.createProfile(inputDto);
 
-            // Assert
             verify(profileValidator).validatorProfileDto(inputDto);
             verify(appUserService).findById(inputDto.getAppUserId());
             verify(profileMapper).toProfile(inputDto);
@@ -107,7 +123,6 @@ import static org.mockito.Mockito.*;
 
         @Test
         void getProfileDto_shouldReturnMappedDtoIfProfileExists() {
-            // Arrange
             Long profileId = 1L;
             Profile mockProfile = new Profile();
             ProfileDto expectedDto = new ProfileDto();
@@ -115,10 +130,8 @@ import static org.mockito.Mockito.*;
             when(profileRepository.findById(profileId)).thenReturn(Optional.of(mockProfile));
             when(profileMapper.toProfileDto(mockProfile)).thenReturn(expectedDto);
 
-            // Act
             ProfileDto result = profileService.getProfileDto(profileId);
 
-            // Assert
             verify(profileRepository).findById(profileId);
             verify(profileMapper).toProfileDto(mockProfile);
 
@@ -128,19 +141,35 @@ import static org.mockito.Mockito.*;
 
         @Test
         void getProfileDto_shouldReturnNullIfProfileDoesNotExist() {
-            // Arrange
             Long profileId = 1L;
 
             when(profileRepository.findById(profileId)).thenReturn(Optional.empty());
 
-            // Act
             ProfileDto result = profileService.getProfileDto(profileId);
 
-            // Assert
             verify(profileRepository).findById(profileId);
             verify(profileMapper, never()).toProfileDto(any());
 
             assertNull(result);
+        }
+        @Test
+        void updatePricing_ShouldReturnAffectedRows() {
+            Set<PriceDto> priceDtoSet = Collections.singleton(new PriceDto());
+            when(priceValidator.validateOrCreatePricing(priceDtoSet)).thenReturn(Collections.singleton(new Price()));
+            when(profileValidator.fetchById(1L)).thenReturn(new Profile());
+
+            Integer result = profileService.updatePricing(priceDtoSet, 1L);
+            assertEquals(1, result);
+        }
+
+        @Test
+        void updateProfileInstrumentQualifications_ShouldReturnAffectedRows() {
+            Set<ProfileInstrumentQualificationDto> qualificationDtoSet = Collections.singleton(new ProfileInstrumentQualificationDto());
+            when(profileValidator.fetchById(1L)).thenReturn(new Profile());
+            when(instrumentService.getInstrumentByIdInternal(anyLong())).thenReturn(new Instrument());
+            when(qualificationService.getQualificationByIdInternal(anyLong())).thenReturn(new Qualification());;
+            Integer result = profileService.updateProfileInstrumentQualifications(1L, qualificationDtoSet);
+            assertEquals(1, result);
         }
     }
 
