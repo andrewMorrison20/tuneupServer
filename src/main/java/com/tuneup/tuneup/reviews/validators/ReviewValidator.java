d9cc99eebf125551ece6cc75a8db64e5a69d8e17
andrewMorrison20 <amorrison20@qub.ac.uk>
2025-04-17 18:40:33 +0100

    refactor region sdto

--- src/main/java/com/tuneup/tuneup/reviews/validators/ReviewValidator.java
+++ src/main/java/com/tuneup/tuneup/reviews/validators/ReviewValidator.java
@@ -2,41 +2,28 @@ package com.tuneup.tuneup.reviews.validators;
 
 import com.tuneup.tuneup.profiles.repositories.ProfileRepository;
 import com.tuneup.tuneup.reviews.dtos.ReviewDto;
-import com.tuneup.tuneup.reviews.model.Review;
 import com.tuneup.tuneup.users.exceptions.ValidationException;
-import com.tuneup.tuneup.users.repository.AppUserRepository;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ReviewValidator {
 
     private final ProfileRepository profileRepository;
-    private final AppUserRepository appUserRepository;
 
-    public ReviewValidator(ProfileRepository profileRepository, AppUserRepository appUserRepository) {
+
+    public ReviewValidator(ProfileRepository profileRepository) {
         this.profileRepository = profileRepository;
-        this.appUserRepository = appUserRepository;
     }
 
     public void  validateReviewDto(ReviewDto reviewDto){
         validateReviewProfile(reviewDto.getProfileId());
-        validateReview(reviewDto.getReviewerProfileId());
+        validateReviewProfile(reviewDto.getReviewerProfileId());
 
     }
 
     private void validateReviewProfile(long profileId) {
-         if(profileRepository.existsById(profileId)){
-             return;
-         } else {
+         if(!profileRepository.existsById(profileId)){
              throw new ValidationException("Profile does not exist");
          }
-    }
-
-    private void validateReview(Long reviewerId) {
-        if(profileRepository.existsById(reviewerId)){
-            return;
-        } else {
-            throw new ValidationException(reviewerId + " is not a registered user");
-        }
     }
 }
