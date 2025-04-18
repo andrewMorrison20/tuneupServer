e93cd72b07b10a651c8d2332807e1116d7d32e32
andrewMorrison20 <amorrison20@qub.ac.uk>
2025-04-18 09:46:39 +0100

    fix workflow

--- src/test/java/com/tuneup/tuneup/TuneupApplicationTests.java
+++ src/test/java/com/tuneup/tuneup/TuneupApplicationTests.java
@@ -2,8 +2,10 @@ package com.tuneup.tuneup;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TuneupApplicationTests {
 
 	@Test
